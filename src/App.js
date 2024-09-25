e23f17fdc0f9e7d1686fd75345b776497d1e617f
ShibalSaekiya ^-^ <laraib776>
2024-09-25 09:38:46 -0700

    Fix redundant alt attribute in project images

--- src/App.js
+++ src/App.js
@@ -17,7 +17,7 @@ import contact from './images/contact.png'
 //import fonts
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faBriefcase, faGraduationCap } from '@fortawesome/free-solid-svg-icons';
-import { faGithub, faLinkedin, faWhatsapp, faTwitter, faFacebook, faLinkedinIn, faGooglePlusG } from '@fortawesome/free-brands-svg-icons';
+import { faGithub,faWhatsapp, faLinkedinIn } from '@fortawesome/free-brands-svg-icons';
 
 function App() {
 
@@ -145,7 +145,7 @@ function App() {
           <a href="https://wa.me/03335550807"><FontAwesomeIcon icon={faWhatsapp} size="2x" className="icon" /></a>
           <a href="https://github.com/laraib776"><FontAwesomeIcon icon={faGithub} size="2x" className="icon" /></a>
           <a href="https://www.linkedin.com/in/laraib-khalid-60b926232"><FontAwesomeIcon icon={faLinkedinIn} size="2x" className="icon" /></a>
-          {/* <a href=""><FontAwesomeIcon icon={faWhatsapp} size="2x" className="icon" /></a> */}
+ 
         </div>
       </div>
    
@@ -187,7 +187,7 @@ function App() {
         >
           <img
             src={project.imageUrl}
-            alt={`${project.title} image`}
+            alt={`${project.title}`}
             style={{
               width: '100%',
               height: '100%',
